f384752db2be2859bdcaa654be5c02cb2645bed3
zax <704912509@qq.com>
2024-01-05 00:17:19 +0800

    feat: header

--- views/inc/header.php
+++ views/inc/header.php
@@ -1,5 +1,47 @@
 <header>
-    <span>dasd</span>
+  <div class="headerTop">
+    <div class="container">
+      <div>
+        <img class="lang" src="" alt="">
+        <span>中文</span>
+        <span>/</span>
+        <span>英文</span>
+      </div>
+      <div>
+        <span>京东</span>
+        <span>|</span>
+        <span>天猫</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="headerContent">
+    <div class="container">
+      <div class="container-left">
+        <img src="" alt="" class="logo1">
+        <div class="line"></div>
+        <img src="" alt="" class="logo2">
+      </div>
+      <div class="container-right">
+        <div class="nav">
+          <div class="navItem">
+            <span class="navName">首页</span>
+            <div class="navSel"></div>
+          </div>
+          <div class="navItem">
+            <span class="navName">产品中心</span>
+          </div>
+          <div class="navItem">
+            <span class="navName">关于我们</span>
+          </div>
+          <div class="navItem">
+            <span class="navName">品牌形象</span>
+          </div>
+        </div>
+        <img src="" alt="" class="search">
+      </div>
+    </div>
+  </div>
 </header>
 
 <script>
