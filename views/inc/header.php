33c6214e2beb928920f9a536d1634f73c484c048
zax <704912509@qq.com>
2024-01-05 17:26:06 +0800

    feat: nav

--- views/inc/header.php
+++ views/inc/header.php
@@ -1,16 +1,18 @@
 <header>
   <div class="headerTop">
     <div class="container">
-      <div>
-        <img class="lang" src="" alt="">
-        <span>中文</span>
-        <span>/</span>
-        <span>英文</span>
+      <div class="container-left">
+        <img class="lang" draggable="false" src="<?php echo static_file('web/img/i18n.webp') ?>" alt="">
+        <span>中文CN</span>
+        <span class="leftLine">/</span>
+        <span>英文EN</span>
       </div>
-      <div>
-        <span>京东</span>
-        <span>|</span>
-        <span>天猫</span>
+      <div class="container-right">
+        <img class="jd" draggable="false" src="<?php echo static_file('web/img/jd.webp') ?>" alt="">
+        <span>京东官方商城</span>
+        <span class="rightLine"></span>
+        <img class="tm" draggable="false" src="<?php echo static_file('web/img/tm.webp') ?>" alt="">
+        <span>天猫官方商城</span>
       </div>
     </div>
   </div>
@@ -18,14 +20,13 @@
   <div class="headerContent">
     <div class="container">
       <div class="container-left">
-        <img src="" alt="" class="logo1">
-        <div class="line"></div>
-        <img src="" alt="" class="logo2">
+        <img class="logo" draggable="false" src="<?php echo static_file('web/img/logo.webp') ?>" alt="">
       </div>
       <div class="container-right">
         <div class="nav">
           <div class="navItem">
             <span class="navName">首页</span>
+            <div class="activeLine"></div>
             <div class="navSel"></div>
           </div>
           <div class="navItem">
@@ -38,7 +39,7 @@
             <span class="navName">品牌形象</span>
           </div>
         </div>
-        <img src="" alt="" class="search">
+        <img class="search" draggable="false" src="<?php echo static_file('web/img/search.webp') ?>" alt="">
       </div>
     </div>
   </div>
