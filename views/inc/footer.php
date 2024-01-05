9521741b8eb4538c107cacaad041975eafc637e1
zax <704912509@qq.com>
2024-01-05 18:56:17 +0800

    footer

--- views/inc/footer.php
+++ views/inc/footer.php
@@ -2,17 +2,19 @@
   <div class="footer-content">
     <div class="container">
       <div class="logoBox">
-        <img src="" alt="" class="logo">
+        <img class="logo" draggable="false" src="<?php echo static_file('web/img/footerlogo.webp') ?>" alt="">
         <div class="share">
-          <img src="" alt="">
-          <img src="" alt="">
-          <img src="" alt="">
+          <div class="shareItem">
+            <img class="weixin" draggable="false" src="<?php echo static_file('web/img/weixin.webp') ?>" alt="">
+          </div>
+          <div class="shareItem">
+            <img class="zheshisha" draggable="false" src="<?php echo static_file('web/img/zheshisha.webp') ?>" alt="">
+          </div>
+          <div class="shareItem">
+            <img class="douyin" draggable="false" src="<?php echo static_file('web/img/douyin.webp') ?>" alt="">
+          </div>
         </div>
-        <div class="cqc">
-          <img src="" alt="">
-          <img src="" alt="">
-          <img src="" alt="">
-        </div>
+        <img class="cqc" draggable="false" src="<?php echo static_file('web/img/cqc.webp') ?>" alt="">
       </div>
       <div class="btmNav">
         <div class="navBox">
@@ -39,23 +41,35 @@
       </div>
       <div class="btmTel">
         <div class="telItem">
-          <p>大叔大婶</p>
-          <p>124123dasda12</p>
+          <p class="p1">大叔大婶</p>
+          <p class="p2">124123dasda12</p>
         </div>
         <div class="telItem">
-          <p>大叔大婶</p>
-          <p>124123dasda12</p>
+          <p class="p1">大叔大婶</p>
+          <p class="p2">124123dasda12</p>
         </div>
         <div class="telItem">
-          <p>大叔大婶</p>
-          <p>124123dasda12</p>
+          <p class="p1">大叔大婶</p>
+          <p class="p2">124123dasda12</p>
+          <p class="p3">salesagwpstools.com</p>
         </div>
       </div>
     </div>
-    <div class="footerBtm">
-      <div class="container">
-
-      </div>
+  </div>
+  <div class="footerBtm">
+    <div class="container">
+      <span>
+        <span>Copyright © 2012-2022 All Rights Reserved 浙ICP备14032411号-1浙公网安备</span>
+        <span> 网站建设：</span>
+        <a href="" target="_blank" rel="noopener noreferrer">博采网络</a>
+      </span>
+      <span>
+        <span>友情链接：</span>
+        <a href="" target="_blank" rel="noopener noreferrer">手动工具产品</a>
+        <a href="" target="_blank" rel="noopener noreferrer">关于我们</a>
+        <a href="" target="_blank" rel="noopener noreferrer">展示中心</a>
+        <a href="" target="_blank" rel="noopener noreferrer">中国五金制品协会</a>
+      </span>
     </div>
   </div>
 </footer>
