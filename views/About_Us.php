c479cf3e3035cc399bf9435b693709fcf65d3f5e
zax <704912509@qq.com>
2024-01-06 21:38:17 +0800

    公司信息

--- views/About_Us.php
+++ views/About_Us.php
@@ -34,6 +34,191 @@ document.oncontextmenu=stop;
         </div>
       </div>
     </div>
+
+    <div class="company">
+      <div class="container">
+
+        <div class="companyLeft">
+          <div class="companyName">宁波长城精工实业有限公司</div>
+          <div class="companyMsg">
+            <p>始创于1984年，是一家集研发、制造、销售、服务于一体的国际化五金工具企业，业务已遍布全球80多个国家和地区，为国内外众多大型工业用户提供完善的工具需求解决方案。</p>
+            <p>历经数十年风雨，长城精工坚持为国内外工具使用者带来有价值的中高档工具。长城精工，成就更多可能！</p>
+          </div>
+          <div class="companyLeftBtm">
+            <div class="companyInfoItem">40年专注于
+              五金工具的研发与制造<span class="infoInex">01</span></div>
+            <div class="companyInfoItem">40年专注于
+              五金工具的研发与制造</div>
+            <div class="companyInfoItem">40年专注于
+              五金工具的研发与制造</div>
+            <div class="companyInfoItem">40年专注于
+              五金工具的研发与制造</div>
+          </div>
+        </div>
+        <div class="companyRight">
+          <div class="numInfoItem">
+            <div class="numBoxWrap">
+              <div class="numBox">
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+              </div>
+              <div class="numUnit">
+                <span>+</span>
+                <span class="unit">个</span>
+              </div>
+            </div>
+            <div class="numInfoName">
+              业务分布
+            </div>
+            <div class="numInfoMsg">
+            拥有现代化的工业厂房12万平方米,占地面积约310亩
+            </div>
+          </div>
+          <div class="numInfoItem">
+            <div class="numBoxWrap">
+              <div class="numBox">
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+              </div>
+              <div class="numUnit">
+                <span>+</span>
+                <span class="unit">个</span>
+              </div>
+            </div>
+            <div class="numInfoName">
+              业务分布
+            </div>
+            <div class="numInfoMsg">
+            拥有现代化的工业厂房12万平方米,占地面积约310亩
+            </div>
+          </div>
+          <div class="numInfoItem">
+            <div class="numBoxWrap">
+              <div class="numBox">
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+              </div>
+              <div class="numUnit">
+                <span>+</span>
+                <span class="unit">个</span>
+              </div>
+            </div>
+            <div class="numInfoName">
+              业务分布
+            </div>
+            <div class="numInfoMsg">
+            拥有现代化的工业厂房12万平方米,占地面积约310亩
+            </div>
+          </div>
+          <div class="numInfoItem">
+            <div class="numBoxWrap">
+              <div class="numBox">
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+                <div class="digit">
+                  <div class="num">0</div>
+                  <div class="num">1</div>
+                  <div class="num">2</div>
+                  <div class="num">3</div>
+                  <div class="num">4</div>
+                  <div class="num">5</div>
+                  <div class="num">6</div>
+                  <div class="num">7</div>
+                  <div class="num">8</div>
+                  <div class="num">9</div>
+                </div>
+              </div>
+              <div class="numUnit">
+                <span>+</span>
+                <span class="unit">个</span>
+              </div>
+            </div>
+            <div class="numInfoName">
+              业务分布
+            </div>
+            <div class="numInfoMsg">
+            拥有现代化的工业厂房12万平方米,占地面积约310亩
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
   <?php include_once VIEWPATH . 'inc/footer.php'; ?>
