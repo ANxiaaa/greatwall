5f9d593813987ae2666e51fc46aca797ebe8719c
zax <704912509@qq.com>
2024-01-11 01:14:00 +0800

    resultTab

--- views/NewsSearch.php
+++ views/NewsSearch.php
@@ -24,6 +24,21 @@ document.oncontextmenu=stop;
         </div>
       </div>
     </div>
+    <div class="resultContent">
+      <div class="container">
+        <div class="resultTitle">
+          <span>共搜索到与“<span class="redText">dasdasd</span>”相关信息，共52条</span>
+        </div>
+        <div class="resultTab">
+          <div class="resultTabItem active">
+            <span>重点信息(20)</span>
+          </div>
+          <div class="resultTabItem">
+            <span>媒体报道(5)</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
   <?php include_once VIEWPATH . 'inc/footer.php'; ?>
