58d544f2b071d8140fe17c094f54543098a95f73
zax <704912509@qq.com>
2024-01-15 03:17:54 +0800

    policies

--- views/Policies.php
+++ views/Policies.php
@@ -13,7 +13,7 @@ document.oncontextmenu=stop;
 <body>
   <?php include_once VIEWPATH . 'inc/header.php'; ?>
 
-  <div class="contactUs">
+  <div class="policies">
     <div class="swiper-container" id="aboutUsBanner">
       <div class="swiper-wrapper" id="aboutUsBanner-wrapper"></div>
     </div>
@@ -34,7 +34,45 @@ document.oncontextmenu=stop;
         </div>
       </div>
     </div>
-
+    <div class="policiesWrap">
+      <div class="container">
+        <div class="topBorder"></div>
+        <div class="policieContent">
+          <div class="policieItem">
+            <div class="policieTitle">
+              <div class="policieNum">一</div>
+              <span>产品标准</span>
+            </div>
+            <div class="policieMsg">① 本公司生产、销售的所有产品均按照有关国际国内标准执行。公司还自订一套比国标，行标更严格．更全面的企业内控检验标淮。
+            ② 为确保标淮的实施，本公司拥有工量具系列的各种测试设备，在生产过程和成品入库前，格按照国家标准GB/2828.1-2012的规定抽检或全检。经检验合格的产品，均具有 “检验合格证”</div>
+          </div>
+          <div class="policieItem">
+            <div class="policieTitle">
+              <div class="policieNum">二</div>
+              <span>产品标准</span>
+            </div>
+            <div class="policieMsg">① 本公司生产、销售的所有产品均按照有关国际国内标准执行。公司还自订一套比国标，行标更严格．更全面的企业内控检验标淮。
+            ② 为确保标淮的实施，本公司拥有工量具系列的各种测试设备，在生产过程和成品入库前，格按照国家标准GB/2828.1-2012的规定抽检或全检。经检验合格的产品，均具有 “检验合格证”</div>
+          </div>
+          <div class="policieItem">
+            <div class="policieTitle">
+              <div class="policieNum">三</div>
+              <span>产品标准</span>
+            </div>
+            <div class="policieMsg">① 本公司生产、销售的所有产品均按照有关国际国内标准执行。公司还自订一套比国标，行标更严格．更全面的企业内控检验标淮。
+            ② 为确保标淮的实施，本公司拥有工量具系列的各种测试设备，在生产过程和成品入库前，格按照国家标准GB/2828.1-2012的规定抽检或全检。经检验合格的产品，均具有 “检验合格证”</div>
+          </div>
+          <div class="policieItem">
+            <div class="policieTitle">
+              <div class="policieNum">十五</div>
+              <span>产品标准</span>
+            </div>
+            <div class="policieMsg">① 本公司生产、销售的所有产品均按照有关国际国内标准执行。公司还自订一套比国标，行标更严格．更全面的企业内控检验标淮。
+            ② 为确保标淮的实施，本公司拥有工量具系列的各种测试设备，在生产过程和成品入库前，格按照国家标准GB/2828.1-2012的规定抽检或全检。经检验合格的产品，均具有 “检验合格证”</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
   <?php include_once VIEWPATH . 'inc/footer.php'; ?>
